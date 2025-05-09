2b6e75705be7ab098b5ea191d5ce1eb3e50ba603
SOURABH SUMAN <sourabhsuman014@gmail.com>
2025-05-09 16:36:35 +0530

    Initial url  commit

--- src/app/enivironment/environmentr.ts
+++ src/app/enivironment/environmentr.ts
@@ -1,4 +1,5 @@
 export class environment
 {
-    static baseurl="https://localhost:7074/api/"
+    // static baseurl="https://localhost:7074/api/"
+    static baseurl="https://application-4pqh.onrender.com/api/"
 }
