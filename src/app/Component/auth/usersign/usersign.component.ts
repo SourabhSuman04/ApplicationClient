d96014d470a741f861eaf13980c801d47c7f7930
SOURABH SUMAN <sourabhsuman014@gmail.com>
2025-05-19 16:28:56 +0530

    localhost problem resolve

--- src/app/Component/auth/usersign/usersign.component.ts
+++ src/app/Component/auth/usersign/usersign.component.ts
@@ -81,7 +81,7 @@ console.log(this.isLoading)
 //   }
 
   loginWithGoogle() {
-    const redirectUrl = 'https://applicationclient.netlify.app'; // Must match your Appwrite config
+    const redirectUrl = 'https://applicationclient.netlify.app/auth/callback'; // Must match your Appwrite config
     this.auth.loginWithOAuth('google', redirectUrl, redirectUrl);
   }
 }
